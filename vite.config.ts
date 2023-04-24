458a6951a64505ceb7a0114ea6156ac958f97df3
Cass Fridkin <cass@swag.lgbt>
2023-04-24 18:04:09 -0500

    add qwik to peerDependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { peerDependencies } from "./package.json";
 
 export default defineConfig(() => {
   return {
@@ -10,6 +11,9 @@ export default defineConfig(() => {
         entry: "./src/index.ts",
         formats: ["es", "cjs"],
         fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
+      },
+      rollupOptions: {
+        external: Object.keys(peerDependencies),
       },
     },
     plugins: [qwikVite(), tsconfigPaths()],
