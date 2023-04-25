ce7517cc10ea7a47564037cfa3c6123d2b93982a
Cass Fridkin <cass@swag.lgbt>
2023-04-25 10:54:52 -0500

    render icons properly using createElement

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,17 +1,28 @@
-import { Fragment, JSXNode, component$ } from "@builder.io/qwik";
-import {
-  icon,
-  parse,
-  AbstractElement,
-  FaSymbol,
-} from "@fortawesome/fontawesome-svg-core";
+import { component$, createElement } from "@builder.io/qwik";
+import { icon, parse } from "@fortawesome/fontawesome-svg-core";
 
 import type {
   IconLookup,
   IconProp,
   SizeProp,
   Transform,
+  AbstractElement,
+  FaSymbol,
+  Attributes,
+  IconParams,
 } from "@fortawesome/fontawesome-svg-core";
+import type { DOMAttributes, Signal, JSXNode } from "@builder.io/qwik";
+
+/**
+ * utility type to ensure i'm not missing anything.
+ *
+ * https://stackoverflow.com/a/57334147
+ */
+type RequireKeys<T> = { [K in keyof T]-?: [T[K]] } extends infer U
+  ? U extends Record<keyof U, [any]>
+    ? { [K in keyof U]: U[K][0] }
+    : never
+  : never;
 
 type IconSpecificProps = {
   beat?: boolean;
@@ -40,46 +51,39 @@ type IconSpecificProps = {
   titleId?: string;
   transform?: string | Transform;
   swapOpacity?: boolean;
+
+  // TODO: once we use DOMAttributes<SVGElement>, replace these
+  class?: DOMAttributes<SVGElement>["class"];
+  // styles?: never // qwik doesn't support inline styles
+  // TODO: inline these once we use DOMAttributes<SVGElement>
+  attributes?: Attributes;
 };
 
 // TODO: `export type IconProps = IconSpecificProps & DOMAttributes<SVGElement>` once https://github.com/literalpie/storybook-framework-qwik/issues/16 is closed
 export type IconProps = IconSpecificProps;
 
-const render = (elements: AbstractElement[]) =>
-  Fragment(
-    {
-      children: elements.map(
-        ({ tag, attributes, children }): JSXNode<string> => {
-          const flags = attributes?.flags ?? 0;
-          const key = attributes.key ?? null;
-
-          return {
-            type: tag,
-            props: attributes,
-            immutableProps: null,
-            children: children ? render(children) : null,
-            flags,
-            key,
-          };
-        }
-      ),
-    },
-    null,
-    0
-  );
-
 export const FaIcon = component$((props: IconProps) => {
   const iconLookup = toIconProp(props.icon);
-  const classes = toClasses(props);
   const transform = toTransform(props.transform);
   const mask = props.mask ? toIconProp(props.mask) : undefined;
 
-  const processedIcon = icon(iconLookup, {
-    ...props,
+  const faClasses = toFaClasses(props);
+  const userClasses = toUserClasses(props.class);
+  const classes = [...faClasses, ...userClasses];
+
+  const params: RequireKeys<IconParams> = {
+    title: props.title,
+    titleId: props.titleId,
     classes,
+    attributes: props.attributes,
+    styles: undefined,
     transform,
+    symbol: props.symbol,
     mask,
-  });
+    maskId: props.maskId,
+  };
+
+  const processedIcon = icon(iconLookup, params);
 
   if (!processedIcon) {
     // error...
@@ -87,8 +91,17 @@ export const FaIcon = component$((props: IconProps) => {
     return null;
   }
 
-  return render(processedIcon.abstract);
+  return <>{render(processedIcon.abstract)}</>;
 });
+
+function render(elements: AbstractElement[]): JSXNode<string>[] {
+  return elements.map(({ tag, attributes, children }) => {
+    const renderedChildren: JSXNode<string>[] = children?.length
+      ? render(children)
+      : [];
+    return createElement(tag, attributes, renderedChildren);
+  });
+}
 
 function toIconProp(icon: IconProp): IconLookup {
   if (Array.isArray(icon)) {
@@ -109,7 +122,7 @@ function toIconProp(icon: IconProp): IconLookup {
   return icon;
 }
 
-function toClasses(props: IconSpecificProps) {
+function toFaClasses(props: IconSpecificProps) {
   const classes = {
     "fa-beat": props.beat,
     "fa-fade": props.fade,
@@ -144,6 +157,35 @@ function toClasses(props: IconSpecificProps) {
   return Object.entries(classes)
     .filter(([, value]) => value)
     .map(([key]) => key);
+}
+
+// rip off `serializeClass`
+function toUserClasses(classes?: IconProps["class"]): string[] {
+  if (typeof classes === "undefined" || classes === null) {
+    return [];
+  }
+
+  if (typeof classes === "string") {
+    return classes.split(/\w+/).map((c) => c.trim());
+  }
+
+  if (isSignal(classes)) {
+    return toUserClasses(classes.value);
+  }
+
+  if (Array.isArray(classes)) {
+    return classes.flatMap((c) => toUserClasses(c));
+  }
+
+  return Object.keys(classes)
+    .filter((key) => classes[key])
+    .map((key) => key.trim());
+}
+
+function isSignal(x: unknown): x is Signal<any> {
+  return (
+    typeof x === "object" && x !== null && x.toString().startsWith("[Signal (")
+  );
 }
 
 function toTransform(
