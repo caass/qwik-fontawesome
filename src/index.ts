f831ff67c6161dc5c8dfc9fcc526b24de71d77b1
Cass Fridkin <cass@swag.lgbt>
2023-04-25 10:55:59 -0500

    not sure how to do css properly yet

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+import { dom, config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
+export { config };
+
 export { FaIcon } from "./components/icon/icon";
+export const faCss = dom.css();
 export type { IconProps } from "./components/icon/icon";
--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,26 @@
+import { faHand } from "@fortawesome/free-regular-svg-icons";
+import { faCss, FaIcon } from ".";
+
 export default () => {
   return (
     <>
       <head>
         <meta charSet="utf-8" />
         <title>Qwik-Fontawesome</title>
+        <style>{faCss}</style>
       </head>
-      <body></body>
+      <body>
+        <div>
+          <p>
+            <span>
+              <FaIcon icon={faHand} pull="left" size="sm" />
+            </span>
+            Hello! This is an example app using{" "}
+            <a href="https://fontawesome.com/">Font Awesome</a> Icons with{" "}
+            <a href="https://qwik.builder.io/">Qwik!</a>
+          </p>
+        </div>
+      </body>
     </>
   );
 };
