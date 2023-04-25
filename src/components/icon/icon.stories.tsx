31cc32ada0966f24d7a11233fada98480516982b
Cass Fridkin <cass@swag.lgbt>
2023-04-25 11:44:10 -0500

    Use stylesheet with storybook

--- src/components/icon/icon.stories.tsx
+++ src/components/icon/icon.stories.tsx
@@ -4,18 +4,21 @@ import type { Meta, StoryObj } from "storybook-framework-qwik";
 import { FaIcon } from "./icon";
 import type { IconProps } from "./icon";
 import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import { faCss } from "../..";
 
 const meta = {
   title: "Icon",
   component: FaIcon,
-  args: {
-    icon: faThumbsUp,
-    size: "8x",
-  },
+  render: (args) => (
+    <>
+      <style dangerouslySetInnerHTML={faCss} />
+      <FaIcon {...args} />
+    </>
+  ),
 } satisfies Meta<IconProps>;
 
 export default meta;
 
 type Story = StoryObj<IconProps>;
 
-export const Static: Story = {};
+export const Static: Story = { args: { icon: faThumbsUp } };
